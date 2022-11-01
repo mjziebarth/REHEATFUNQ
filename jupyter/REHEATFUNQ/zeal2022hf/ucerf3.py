26d15877ecb49d3adc85e50df536860d39626d22
Malte J. Ziebarth <ziebarth@gfz-potsdam.de>
2022-11-01 13:18:56 +0100

    Add UCERF3 result class from Ziebarth et al, 2022 code.

--- jupyter/REHEATFUNQ/zeal2022hf/ucerf3.py
+++ jupyter/REHEATFUNQ/zeal2022hf/ucerf3.py
@@ -1,4 +1,4 @@
-# Heatflow paper common code.
+# Result of the UCERF3 branch analysis.
 #
 # This file is part of the ziebarth_et_al_2022_heatflow python module.
 #
@@ -19,9 +19,18 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from .geojson import write_polygon_geojson
-from .geopackage import read_geopackage_polys
-from .ucerf3 import UCERF3BranchResult
-from .plottools import get_cm_colors, kindlmann, kindlmann_r
-#from .crust1 import CRUST1
-#from .nghf import read_nghf
+class UCERF3BranchResult:
+    """
+    This class contains the result of the analysis of one
+    UCERF3 branch.
+    """
+    def __init__(self, weight, saf_relative_power, saf_power, total_power,
+                 fault_power, ld2v, d2, sr):
+        self.weight = weight
+        self.saf_relative_power = saf_relative_power
+        self.saf_power = saf_power
+        self.total_power = total_power
+        self.fault_power = fault_power
+        self.ld2v = ld2v
+        self.d2 = d2
+        self.sr = sr
