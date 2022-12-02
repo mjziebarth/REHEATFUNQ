62ff574d91471600a0fda27ba7a24e98aaef1b7c
Malte J. Ziebarth <ziebarth@gfz-potsdam.de>
2022-12-02 17:36:00 +0100

    Change unit notation in GammaConjugatePrior visualization.

--- reheatfunq/regional/prior.py
+++ reheatfunq/regional/prior.py
@@ -558,7 +558,7 @@ class GammaConjugatePrior:
                     a_plot[(a_plot >= qamin) & (a_plot <= qamax)] / q,
                     color=color, linewidth=0.7, zorder=1)
             ax.text(sqrt(qamin * qamax), 1.2 * sqrt(qamin * qamax) / q,
-                    f'${q}\,\\mathrm{{mW}}/\\mathrm{{m}}^2$', fontsize=8,
+                    f'${q}\,\\mathrm{{mWm}}^{{-2}}$', fontsize=8,
                     rotation=45, color=color)
 
         # 3) The standard deviation isolines:
@@ -576,7 +576,7 @@ class GammaConjugatePrior:
                     linestyle=':', color=color, linewidth=0.7)
             albl = sqrt(qamin * qamax)
             blbl = sqrt(albl) / qstd
-            ax.text(albl, 1.1*blbl, f'${qstd}\,\\mathrm{{mW}}/\\mathrm{{m}}^2$',
+            ax.text(albl, 1.1*blbl, f'${qstd}\,\\mathrm{{mWm}}^{{-2}}$',
                     fontsize=8, rotation=27, color=color)
 
         if ai is not None:
