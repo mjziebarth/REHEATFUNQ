0aae6b8828485d7b3dfcae175a256d57dc45d7f4
Malte J. Ziebarth <ziebarth@gfz-potsdam.de>
2023-01-17 20:46:57 +0100

    Fix file path.

--- vendor/patch/python/test_bz2.py
+++ vendor/patch/python/test_bz2.py
@@ -1,5 +1,5 @@
 #!/bin/python
 # This file tests whether the bzip2 import works.
 import bz2
-with bz2.open('vendor/xz-5.4.0.tar.bz2','r') as f:
+with bz2.open('vendor/compile/xz-5.4.0.tar.bz2','r') as f:
     pass
