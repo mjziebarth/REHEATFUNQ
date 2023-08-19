3aae41c0a1b4cb7bcd0cbdf2d2c06c57f6565543
Malte Jörn Ziebarth <malte.ziebarth@fmvkb.de>
2023-08-19 15:38:51 +0200

    The quest to prison break (3)

--- .github/scripts/numpy-include.py
+++ .github/scripts/numpy-include.py
@@ -1,8 +1,10 @@
-print()
+print("")
 print("==== numpy include ====")
+import sys
+print("path:",sys.path)
 import numpy as np
 incl = np.get_include()
 print(incl)
 
 print("==== numpy include ====")
-print()
+print("")
