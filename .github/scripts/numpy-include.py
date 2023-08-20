bb3057dc0c3b6c752e4105b21235f001f2ca934f
Malte Jörn Ziebarth <malte.ziebarth@fmvkb.de>
2023-08-20 13:57:13 +0200

    The quest to prison break (10)

--- .github/scripts/numpy-include.py
+++ .github/scripts/numpy-include.py
@@ -6,6 +6,9 @@ print("path:",sys.path)
 import numpy as np
 import subprocess
 print(subprocess.check_output(
+    ['python','-c','import sys; print(sys.path)'],
+    env={}))
+print(subprocess.check_output(
     ['python','-c','import os; import numpy; print(numpy.get_include())'],
     env={}))
 sleep(1)
