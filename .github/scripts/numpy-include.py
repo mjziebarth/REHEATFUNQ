da4a43ef06576a67195d680514938e229a75c229
Malte Jörn Ziebarth <malte.ziebarth@fmvkb.de>
2023-08-20 13:18:58 +0200

    Query python executable path.

--- .github/scripts/numpy-include.py
+++ .github/scripts/numpy-include.py
@@ -3,6 +3,8 @@ print("==== numpy include ====")
 import sys
 print("path:",sys.path)
 import numpy as np
+import subprocess
+subprocess.run(['whereis', 'python'])
 incl = np.get_include()
 print(incl)
 
