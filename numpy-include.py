3728b2aedd319197f6e600aba53c99d11a62acf1
Malte J. Ziebarth <ziebarth@gfz-potsdam.de>
2023-08-21 11:32:43 +0200

    The quest to prison break (16)

--- numpy-include.py
+++ numpy-include.py
@@ -8,9 +8,30 @@ try:
 
 except ImportError:
     import subprocess
+    import sys
+    from pathlib import Path
+    from os import rename, symlink
     np_incl_byte = subprocess.check_output(
         ['python','-c','import numpy; print(numpy.get_include())'],
         env={}
     )
 
-    print(np_incl_byte.decode().strip())
+    np_include = np_incl_byte.decode().strip()
+
+    # Make numpy available in this isolated path:
+    success = False
+    for path in sys.path[::-1]:
+        if 'site-packages' in path:
+            p = Path(path)
+            is_dir = (p / "numpy").is_dir()
+            if is_dir:
+                rename((p / "numpy").resolve(), (p / "numpyold").resolve())
+            os.symlink(np_include, (p / "numpy").resolve())
+            success = True
+            break
+
+    if not success:
+        raise RuntimeError("Could not link the NumPy package to the isolated "
+                           "site-packages.")
+
+    print(np_include)
