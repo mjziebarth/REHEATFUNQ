0624c253a1630976371c3df59e80f73cc4112843
Malte J. Ziebarth <ziebarth@gfz-potsdam.de>
2024-01-02 17:27:37 +0100

    Ensure that all Cython code imports without raising undefined symbol exceptions.

--- testing/test_import.py
+++ testing/test_import.py
@@ -24,3 +24,22 @@ def test_api_import():
     from reheatfunq.coverings import random_global_R_disk_coverings,\
                            bootstrap_data_selection, conforming_data_selection
     from reheatfunq.regional import gamma_mle
+
+
+def test_cython_import():
+    """
+    This function tests whether any of the compiled Cython modules
+    throw an import error when imported from. This usually means that
+    there is an undefined symbol and meson.build or the source code
+    has to be updated.
+    """
+    from reheatfunq.regional.backend import gamma_conjugate_prior_mle
+    from reheatfunq.anomaly.bayes import marginal_posterior_tail
+    from reheatfunq.anomaly.postbackend import CppAnomalyPosterior
+    from reheatfunq.coverings.rdisks import all_restricted_samples
+    from reheatfunq.coverings.poisampling import generate_point_of_interest_sampling
+    from reheatfunq.anomaly.anomaly import Anomaly
+    from reheatfunq.resilience.zeal2022hfresil \
+        import generate_synthetic_heat_flow_coverings_mix2
+    from reheatfunq.data.distancedistribution import distance_distribution
+    from reheatfunq._testing.barylagrint import BarycentricLagrangeInterpolator
